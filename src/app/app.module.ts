310830023239d7a6c044b37ea615906eab79eabe
oscar-quiroz <kairox.rt@gmail.com>
2021-05-25 13:12:48 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, PrimeNgModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}

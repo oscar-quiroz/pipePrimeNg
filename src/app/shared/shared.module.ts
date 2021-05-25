24f3cde63d59d11a524d9694ece92d00993a7e4b
oscar-quiroz <kairox.rt@gmail.com>
2021-05-25 13:27:47 -0500

    menu bar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
-
-
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
 @NgModule({
-  declarations: [
-    MenuComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [MenuComponent],
+  exports: [MenuComponent],
+  imports: [CommonModule, PrimeNgModule],
 })
-export class SharedModule { }
+export class SharedModule {}

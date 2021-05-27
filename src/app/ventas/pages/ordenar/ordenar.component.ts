d6305bcd312d96fff71216da51ab71a8c2817f13
oscar-quiroz <kairox.rt@gmail.com>
2021-05-27 14:00:15 -0500

    pipe de vuela

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Heore } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -7,8 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdenarComponent implements OnInit {
   texto: string = 'nosotros';
+  enMayuscula: boolean = true;
+
+  heroes: Heore[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul,
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro,
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde,
+    },
+    {
+      nombre: 'DaredDevil',
+      vuela: false,
+      color: Color.rojo,
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde,
+    },
+  ];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  change() {
+    if (this.enMayuscula) {
+      this.enMayuscula = false;
+    } else {
+      this.enMayuscula = true;
+    }
+  }
 }
